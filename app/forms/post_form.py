1da5ecaccb2942e8cac59ec8be295e99f0f172ed
katherin <katherin.garciaVA1@gmail.com>
2023-02-21 18:20:21 -0500

    database

---
+++ app/forms/post_form.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError
+
+def name_length(form, field):
+    name = field.data
+    if len(name) < 3:
+        raise ValidationError('Name must be at least 3 characters.')
+    
+
+def description_length(form, field):
+    description = field.data
+    if len(description) > 2500 or len(description) < 3:
+        raise ValidationError('Description must be between 3 and 2500 characters long.')
+    
+def images_url_valid(form, field):
+    imageUrl = field.data
+    if not imageUrl.endswith(('.png', '.jpg', '.gif')):
+        raise ValidationError('URL does not contain an image.')
+
+
+class PostForm(FlaskForm):
+	name= StringField('name', validators=[DataRequired(), name_length])
+	description= TextAreaField('description', validators=[DataRequired(), description_length])
+	imageUrl = StringField('imageUrl' , validators=[DataRequired(), images_url_valid])
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -3,6 +3,16 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
+def valid_first_name(form,field):
+    firstName = field.data
+    if len(firstName)< 2 or len(firstName) >20:
+        raise ValidationError('First name must be between 2 and 20 characters')
+
+def valid_last_name(form, field):
+    lastName = field.data
+
+    if len(lastName)< 2 or len(lastName) > 20:
+        raise ValidationError('Last name must be between 2 and 20 characters')
 
 def user_exists(form, field):
     # Checking if user exists
@@ -21,6 +31,8 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
+    firstName = StringField('firstName', validators=[DataRequired(), valid_first_name] )
+    lastName = StringField('lastName', validators=[DataRequired(), valid_last_name])
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
