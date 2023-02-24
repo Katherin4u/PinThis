501cafc1118d3146511e122b3c4d3ae78288147f
katherin <katherin.garciaVA1@gmail.com>
2023-02-24 10:56:14 -0500

    all comments

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from .models import db, User, Post
+from .models import db, User, Post, Comment
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.post_routes import post_routes
+from .api.comment_routes import comment_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -30,6 +31,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(post_routes, url_prefix='/api/posts')
+app.register_blueprint(comment_routes, url_prefix='/api/comments')
 db.init_app(app)
 Migrate(app, db)
 
---
+++ react-app/src/store/comments.js
@@ -0,0 +1,152 @@
+const LOAD_ALL_COMMENTS = 'comments/LOAD_ALL_COMMENTS'
+const CREATE_COMMENT = 'comments/CREATE_COMMENT';
+const UPDATE_COMMENT = 'comments/UPDATE_COMMENT';
+const DELETE_COMMENT = 'comments/DELETE_COMMENT';
+const CLEAN_UP_COMMENTS = 'comments/CLEANUP_COMMENTS';
+
+export const loadAllComments = (comments, postId) => {
+    return {
+        type: LOAD_ALL_COMMENTS,
+        postId,
+        comments
+    }
+};
+
+export const createCommentAction = (postId, newComment) => {
+    return {
+        type: CREATE_COMMENT,
+        postId,
+        newComment
+    }
+};
+
+export const updateCommentAction = (updatedComment) => {
+    return {
+        type: UPDATE_COMMENT,
+        updatedComment
+    }
+};
+
+export const deleteCommentAction = (badCommentId) => {
+    return {
+        type: DELETE_COMMENT,
+        badCommentId
+    }
+};
+
+export const cleanUpCommentAction = () => {
+    return {
+        type: CLEAN_UP_COMMENTS
+    }
+};
+
+
+export const loadAllCommentsThunk = (postId) => async dispatch => {
+    const response = await fetch(`/api/posts/${postId}/comments`);
+
+    if (response.ok) {
+        const allComments = await response.json();
+        dispatch(loadAllComments(postId, allComments));
+        return allComments;
+    }
+}
+
+
+export const createCommentThunk = (postId, newComment) => async dispatch => {
+    console.log(newComment)
+    const response = await fetch(`/api/posts/${postId}/comments`, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newComment),
+    });
+
+    if (response.ok) {
+        const createdComment = await response.json()
+        dispatch(createCommentAction(createdComment))
+        return createdComment;
+    } else if (response.status < 500) {
+        const createdComment = await response.json()
+        if (createdComment.errors) {
+            return createdComment.errors;
+        }
+    } else {
+        return ["An error occurred. Please try again."];
+    }
+
+};
+
+export const updateCommentThunk = (comments) => async dispatch => {
+    const response = await fetch(`/api/comments/${comments.id}`, {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(comments),
+    });
+
+    if (response.ok) {
+        const updatedComment = await response.json()
+        dispatch(createCommentAction(updatedComment))
+        return updatedComment;
+    } else if (response.status < 500) {
+        const updatedComment = await response.json()
+        if (updatedComment.errors) {
+            return updatedComment.errors;
+        }
+    } else {
+        return ["An error occurred. Please try again."];
+    }
+
+};
+
+export const deleteCommentThunk = (badCommentId) => async dispatch => {
+    const response = await fetch(`/api/comments/${badCommentId}`, {
+        method: "DELETE"
+    });
+
+    if (response.ok) {
+        const badComment = await response.json();
+        dispatch(deleteCommentAction(badCommentId));
+        return badComment;
+    }
+};
+
+const normalize = (arr) => {
+    const resObj = {}
+    arr.forEach((ele) => { resObj[ele.id] = ele })
+    return resObj
+}
+
+//reviews reducer
+const initialState = {};
+
+const commentsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_ALL_COMMENTS: {
+            const newState = { ...state };
+            newState.postComments = normalize(action.postId)
+            return newState;
+        };
+        case CREATE_COMMENT: {
+            const newState = { ...state };
+            newState.postComments[action.postId.id] = action.postId;
+            return newState;
+        };
+        case UPDATE_COMMENT: {
+            const newState = { ...state };
+            newState.comments.postComments[action.updatedComment.id] = action.updatedComment;
+            return newState;
+        };
+        case DELETE_COMMENT: {
+            const newState = { ...state };
+            delete newState.postComments[action.badCommentId];
+            return newState;
+        };
+        case CLEAN_UP_COMMENTS: {
+            const newState = { ...initialState };
+            return newState;
+        }
+        default:
+            return state;
+    }
+};
+
+export default commentsReducer;
