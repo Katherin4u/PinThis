a3d32dd74a2bdb940cfe76012774a4be9a2e5dcc
katherin <katherin.garciaVA1@gmail.com>
2023-02-27 21:17:40 -0500

    render testing

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .api.post_routes import post_routes
 from .api.images_route import image_routes
 from .api.comment_routes import comment_routes
+from .api.search_route import search_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -34,6 +35,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(post_routes, url_prefix='/api/posts')
 app.register_blueprint(comment_routes, url_prefix='/api/comments')
 app.register_blueprint(image_routes, url_prefix='/api/images')
+app.register_blueprint(search_routes, url_prefix='/api/search')
 db.init_app(app)
 Migrate(app, db)
 
